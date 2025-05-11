776a856c4f09e19fde665bbddedec4140269a323
Alex <79389493+alexelkinx@users.noreply.github.com>
2025-05-11 19:40:11 +0300

    Add environment variable support to config file

--- backend/config/config.php
+++ backend/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'events');
-define('DB_USER', 'admin');
-define('DB_PASS', 'asennus');
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_NAME', getenv('DB_NAME') ?: 'turku_events');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
